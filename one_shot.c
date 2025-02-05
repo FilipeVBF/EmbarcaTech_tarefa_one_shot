09a3597ae2f2eac400819edb51dec2ea8d88e161
FilipeVBF <filipevbf@gmail.com>
2025-02-04 21:41:51 -0300

    ajuste na cor do led

--- one_shot.c
+++ one_shot.c
@@ -2,9 +2,9 @@
 #include "pico/stdlib.h" // Biblioteca padrão do Raspberry Pi Pico para controle de GPIO, temporização e comunicação serial.
 #include "pico/time.h"   // Biblioteca para gerenciamento de temporizadores e alarmes.
 
-// Define os pinos GPIO para os LEDs vermelho, amarelo e verde.
-const uint LED_PIN_RED = 11;
-const uint LED_PIN_YELLOW = 12;
+// Define os pinos GPIO para os LEDs azul, vermelho e verde.
+const uint LED_PIN_BLUE = 11;
+const uint LED_PIN_RED = 12;
 const uint LED_PIN_GREEN = 13;
 const uint BUTTON_PIN = 5;  // Define o pino GPIO 5 para ler o estado do botão.
 
@@ -29,21 +29,21 @@ int64_t turn_off_green_callback(alarm_id_t id, void *user_data) {
     return 0;
 }
 
-int64_t turn_off_yellow_callback(alarm_id_t id, void *user_data) {
+int64_t turn_off_red_callback(alarm_id_t id, void *user_data) {
 
-    gpio_put(LED_PIN_YELLOW, false);
+    gpio_put(LED_PIN_RED, false);
 
     add_alarm_in_ms(3000, turn_off_green_callback, NULL, false);
 
     return 0;
 }
 
-int64_t turn_off_red_callback(alarm_id_t id, void *user_data) {
+int64_t turn_off_blue_callback(alarm_id_t id, void *user_data) {
 
     // Desliga o LED configurando o pino LED_PIN para nível baixo.
-    gpio_put(LED_PIN_RED, false);
+    gpio_put(LED_PIN_BLUE, false);
 
-    add_alarm_in_ms(3000, turn_off_yellow_callback, NULL, false);
+    add_alarm_in_ms(3000, turn_off_red_callback, NULL, false);
 
     // Retorna 0 para indicar que o alarme não deve se repetir.
     return 0;
@@ -53,8 +53,8 @@ int main() {
     stdio_init_all();
 
     // Inicializando pino do LED RGB
+    init_gpio_led(LED_PIN_BLUE, true, false);
     init_gpio_led(LED_PIN_RED, true, false);
-    init_gpio_led(LED_PIN_YELLOW, true, false);
     init_gpio_led(LED_PIN_GREEN, true, false);
 
     // Configura o pino BUTTON_PIN (5) como entrada digital.
@@ -74,15 +74,15 @@ int main() {
             // Verifica novamente o estado do botão após o debounce.
             if (gpio_get(BUTTON_PIN) == 0) {
                 // Acende o LED configurando o pino LED_PIN para nível alto.
+                gpio_put(LED_PIN_BLUE, true);
                 gpio_put(LED_PIN_RED, true);
-                gpio_put(LED_PIN_YELLOW, true);
                 gpio_put(LED_PIN_GREEN, true);
 
                 // Define 'led_active' como true para indicar que o LED está aceso.
                 led_active = true;
 
                 // Agenda um alarme para desligar o LED após 3 segundos (3000 ms).
-                add_alarm_in_ms(3000, turn_off_red_callback, NULL, false);
+                add_alarm_in_ms(3000, turn_off_blue_callback, NULL, false);
             }
         }
 
